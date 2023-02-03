c7c78ddd95cd7d0a5f51fc20d7bc43dc07f86d1c
Mia Harang <miharang@iastate.edu>
2023-02-03 15:23:05 -0600

    maven and springboot projects

---
+++ Experiments/Mia Harang/Tutorials/demo_1/src/test/java/com/coms309/demo_1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coms309.demo_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
