1cb7a32f79c710897fdfeb9dd7f5f2625e8bbdd0
Mia Harang <miharang@iastate.edu>
2023-02-03 16:22:58 -0600

    experimentation

---
+++ Experiments/Mia Harang/Experimentation/spring_exp/src/test/java/com/coms309/spring_exp/SpringExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coms309.spring_exp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
