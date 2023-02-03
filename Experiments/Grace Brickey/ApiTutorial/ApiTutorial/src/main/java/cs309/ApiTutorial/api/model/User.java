0059f893737c47283cc223f972f6cee0dda0aac3
gbrickey <gbrickey@iastate.edu>
2023-02-03 15:00:54 -0600

    Connecting to postman

--- Experiments/Grace Brickey/ApiTutorial/ApiTutorial/src/main/java/cs309/ApiTutorial/api/model/User.java
+++ Experiments/Grace Brickey/ApiTutorial/ApiTutorial/src/main/java/cs309/ApiTutorial/api/model/User.java
@@ -16,4 +16,52 @@ public class User {
         this.usertype = usertype;
         this.wins = 0;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
 }
--- Experiments/Grace Brickey/ApiTutorial/ApiTutorial/src/main/java/cs309/ApiTutorial/service/UserService.java
+++ Experiments/Grace Brickey/ApiTutorial/ApiTutorial/src/main/java/cs309/ApiTutorial/service/UserService.java
@@ -1,4 +1,39 @@
 package cs309.ApiTutorial.service;
 
+
+import cs309.ApiTutorial.api.model.User;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private ArrayList<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<User>();
+
+        User user = new User(1,"Grace",21,"gbrickey@iastate.edu","Administer");
+        User user1 = new User(2,"Dan",18,"dancoolio@iastate.edu","General");
+        User user2 = new User(3,"Phil",11,"philyomama@iastate.edu","General");
+        User user3 = new User(4,"Emily",28,"emilydickens@iastate.edu","Premium");
+        User user4 = new User(5,"Leah",22,"lgiese@iastate.edu","Administer");
+
+        userList.addAll(Arrays.asList(user,user1,user2,user3,user4));
+    }
+
+    public Optional<User> getUser(Integer id) {
+        Optional optional = Optional.empty();
+        for (User user : userList) {
+            if(id == user.getId()) {
+                optional = Optional.of(user);
+                return optional;
+            }
+        }
+        return optional;
+    }
 }
