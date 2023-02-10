ba6975977ea2bef1bb9950c026b8a7480176bc51
Grace Brickey <gbrickey@iastate.edu>
2023-02-10 23:18:07 +0000

    Upload New File

---
+++ Backend/clue_backend/src/main/java/tz_7/UserModel/User.java
@@ -0,0 +1,100 @@
+package coms309.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+    private String type; //use type can be a for administer, b for basic
+                            // and p for premium
+    private int wins;
+
+//    public User(int id, String firstname, String lastname, String email, String password, String type, int wins) {
+//        this.id = id;
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.email = email;
+//        this.password = password;
+//        this.type = type;
+//        this.wins = wins;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    public int getWins() {
+//        return wins;
+//    }
+//
+//    public void setWins(int wins) {
+//        this.wins = wins;
+//    }
+}
