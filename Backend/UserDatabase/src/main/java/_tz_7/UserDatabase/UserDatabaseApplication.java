d78a59ebb4d33269d1337a8e9506e8192fc10ea9
gbrickey <gbrickey@iastate.edu>
2023-02-10 13:10:14 -0600

    User Database project folder

---
+++ Backend/UserDatabase/src/main/java/_tz_7/UserDatabase/UserDatabaseApplication.java
@@ -0,0 +1,13 @@
+package _tz_7.UserDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDatabaseApplication.class, args);
+	}
+
+}
