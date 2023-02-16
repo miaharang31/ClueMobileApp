9a676122f3e8fbc384bc07a6c9fb2e0e429df4b1
Mia Harang <miharang@iastate.edu>
2023-02-15 23:40:49 -0600

    does not run anything, but runs without errors

--- Backend/clue_backend/src/main/java/tz_7/ClueApplication.java
+++ Backend/clue_backend/src/main/java/tz_7/ClueApplication.java
@@ -3,14 +3,6 @@ package tz_7;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * PetClinic Spring Boot Application.
- *
- * @author Dave Syer
- * Modified by Tanmay Ghosh
- * Modified by Vivek Bengre
- */
-
 @SpringBootApplication
 public class ClueApplication {
 
--- Backend/clue_backend/src/main/java/tz_7/UserDatabase/dao/UserRepository.java
+++ Backend/clue_backend/src/main/java/tz_7/UserDatabase/dao/UserRepository.java
@@ -1,10 +1,10 @@
-package coms309.dao;
-
-import coms309.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findByType(String type);
-}
+//package tz_7.UserDatabase.dao;
+//
+//import coms309.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface UserRepository extends JpaRepository<User, Integer> {
+//    List<User> findByType(String type);
+//}
