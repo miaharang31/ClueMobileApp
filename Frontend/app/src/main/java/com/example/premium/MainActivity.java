f53e0240d21f68c9906eebb58457eddf0316f71e
emrsal23 <esalsman@iastate.edu>
2023-02-23 18:30:00 -0600

    adding premium page

--- Frontend/app/src/main/java/com/example/premium/MainActivity.java
+++ Frontend/app/src/main/java/com/example/premium/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.premium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +20,19 @@ public class MainActivity extends AppCompatActivity {
         standard = findViewById(R.id.StandardButton);
         premium = findViewById(R.id.PremiumButton);
 
+        premium.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, PremiumActivity.class);
+                startActivity(i);
+            }
+        });
 
-
+        standard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(MainActivity.this, StandardActivity.class);
+            }
+        });
     }
 }
---
+++ Frontend/app/src/main/java/com/example/premium/PremiumActivity.java
@@ -0,0 +1,4 @@
+package com.example.premium;
+
+public class PremiumActivity {
+}
