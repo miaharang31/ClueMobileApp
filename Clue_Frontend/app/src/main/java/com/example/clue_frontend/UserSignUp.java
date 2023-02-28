939157f4472ac946df03c44da76b08f21a1e67d2
Piper Ideker <plideker@iastate.edu>
2023-02-27 20:22:56 -0600

    added home with navigation drawer

--- Clue_Frontend/app/src/main/java/com/example/clue_frontend/UserSignUp.java
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/UserSignUp.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +18,9 @@ public class UserSignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_user_sign_up);
 
         Button signUpButton = (Button) findViewById(R.id.signUpButton);
