a9343fedea247d90547917adab8af824ce5809f7
Mia Harang <miharang@iastate.edu>
2023-02-27 23:04:35 -0600

    host lobby recreation

--- Clue_Frontend/app/src/main/java/com/example/clue_frontend/MainActivity.java
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/MainActivity.java
@@ -20,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         Button submitButton = (Button) findViewById(R.id.submitButton);
         TextView newPlayerLink = findViewById(R.id.NewPlayerLink);
 
+//      MIA USED FOR TESTING
+//        submitButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, HostLobby.class);
+//                startActivity(intent);
+//            }
+//        });
+
         //When the sign in button is clicked and data is entered for usename and password
         //submitButton.setOnClickListener(new View.OnClickListener() {
 
