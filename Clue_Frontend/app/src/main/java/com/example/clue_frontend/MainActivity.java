2fdc65c2aba562671edc9a2473752305a8d61050
gbrickey <gbrickey@iastate.edu>
2023-03-07 01:21:50 -0600

    got scrollbar and different look to the rules page

--- Clue_Frontend/app/src/main/java/com/example/clue_frontend/MainActivity.java
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -25,13 +26,15 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //activity_main
+
 
         Button submitButton = (Button) findViewById(R.id.submitButton);
         TextView newPlayerLink = findViewById(R.id.NewPlayerLink);
