f5b5f500b9128e4f2f5e1cac1e4a05d91262c2dd
Piper Ideker <plideker@iastate.edu>
2023-03-22 20:21:05 -0500

    four out of ten rooms on the board

--- BoardTest/app/src/main/java/com/example/boardtest/GameView.java
+++ BoardTest/app/src/main/java/com/example/boardtest/GameView.java
@@ -12,23 +12,36 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 
 public class GameView extends View {
-    private Bitmap bm1, bm2;
+    private Bitmap tile1, tile2, study, library, billiard, conservatory,
+    hall;
     public static int sizeOfMap = 35 * Constraints.SCREEN_WIDTH/1000;
     private int h = 20, w = 20;
     private ArrayList<Board> arrBoard = new ArrayList<>();
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        bm1 = BitmapFactory.decodeResource(this.getResources(),R.drawable.dark);
-        bm1 = Bitmap.createScaledBitmap(bm1, sizeOfMap, sizeOfMap, true);
-        bm2 = BitmapFactory.decodeResource(this.getResources(),R.drawable.light);
-        bm2 = Bitmap.createScaledBitmap(bm2, sizeOfMap, sizeOfMap, true);
+        tile1 = BitmapFactory.decodeResource(this.getResources(),R.drawable.dark);
+        tile1 = Bitmap.createScaledBitmap(tile1, sizeOfMap, sizeOfMap, true);
+        tile2 = BitmapFactory.decodeResource(this.getResources(),R.drawable.light);
+        tile2 = Bitmap.createScaledBitmap(tile2, sizeOfMap, sizeOfMap, true);
+
+        study = BitmapFactory.decodeResource(this.getResources(), R.drawable.study);
+        study = Bitmap.createScaledBitmap(study, 242, 205, true);
+        library = BitmapFactory.decodeResource(this.getResources(), R.drawable.library);
+        library = Bitmap.createScaledBitmap(library, 266, 230, true);
+        billiard = BitmapFactory.decodeResource(this.getResources(), R.drawable.billiard_room);
+        billiard = Bitmap.createScaledBitmap(billiard, 282, 174, true);
+        conservatory = BitmapFactory.decodeResource(this.getResources(), R.drawable.conservatory);
+        conservatory = Bitmap.createScaledBitmap(conservatory, 166, 205, true);
+        hall = BitmapFactory.decodeResource(this.getResources(), R.drawable.hall);
+        hall = Bitmap.createScaledBitmap(hall, 302, 212, true);
+
         for(int i = 0; i < h; i++){
             for(int j = 0; j < w; j++){
                 if((i+j)%2 ==0){
-                    arrBoard.add(new Board(bm1,j*sizeOfMap + Constraints.SCREEN_WIDTH/2-(w/2)*sizeOfMap,
+                    arrBoard.add(new Board(tile1,j*sizeOfMap + Constraints.SCREEN_WIDTH/2-(w/2)*sizeOfMap,
                             i * sizeOfMap + 500 * Constraints.SCREEN_HEIGHT/1920,sizeOfMap,sizeOfMap));
                 }else{
-                    arrBoard.add(new Board(bm2,j*sizeOfMap + Constraints.SCREEN_WIDTH/2-(w/2)*sizeOfMap,
+                    arrBoard.add(new Board(tile2,j*sizeOfMap + Constraints.SCREEN_WIDTH/2-(w/2)*sizeOfMap,
                             i * sizeOfMap + 500 * Constraints.SCREEN_HEIGHT/1920,sizeOfMap,sizeOfMap));
                 }
             }
@@ -38,10 +51,15 @@ public class GameView extends View {
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        canvas.drawColor(0xFFFFFF);
         for (int i = 0; i < arrBoard.size(); i++){
             canvas.drawBitmap(arrBoard.get(i).getBm(), arrBoard.get(i).getX(),
                     arrBoard.get(i).getY(), null);
         }
+
+        canvas.drawBitmap(study, 156,546, null);
+        canvas.drawBitmap(library, 149,678, null);
+        canvas.drawBitmap(billiard, 158,911, null);
+        canvas.drawBitmap(conservatory, 159,1101, null);
+        canvas.drawBitmap(hall, 488,548, null);
     }
 }
