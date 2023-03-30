3f3b58a48289ed4ac2561a94e4edc174bd389cfa
Mia Harang <miharang@iastate.edu>
2023-03-30 16:52:31 -0500

    oops forgot to commit :( dont yell at me

--- Backend/clue_backend/src/main/java/tz_7/GamePlay/GameStateDatabase/GameState.java
+++ Backend/clue_backend/src/main/java/tz_7/GamePlay/GameStateDatabase/GameState.java
@@ -5,6 +5,12 @@ import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 
 import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Author: Mia Harang
+ * Database that holds the information of all the current games in session
+ */
 
 @Entity
 @Table(name = "GameState")
@@ -13,59 +19,113 @@ public class GameState {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", unique = true)
     @NotFound(action = NotFoundAction.IGNORE)
+    /**
+     * Game ID
+     */
     private Integer ID;
+
     @Column(name = "versionID")
     @NotFound(action = NotFoundAction.IGNORE)
+    /**
+     * The ID of the version of game that it is
+     * i.e basic or some sort of premium game (held in separate table)
+     */
     private Integer versionID;
+
     @Column(name = "lobbyID")
     @NotFound(action = NotFoundAction.IGNORE)
+    /**
+     * ID of the lobby that everyone came from
+     */
     private Integer lobbyID;
+
     @Column(name = "finalCardIDs")
     @NotFound(action = NotFoundAction.IGNORE)
+    /**
+     * IDs of the cards that are "put in the center"
+     * Used to check if the final guess of a player is correct
+     */
     private Integer[] finalCardIDs;
-    @Column(name = "turnOrder")
-    @NotFound(action = NotFoundAction.IGNORE)
-    private ArrayList<Integer> turnOrder;
 
-//    TODO: FIND A NEW WAY TO DO PLAYER HAND
-//    @Column(name = "playerHand")
+//    @Column(name = "abilityDeck")
 //    @NotFound(action = NotFoundAction.IGNORE)
-//    private ArrayList<ArrayList<Integer>> playerHand;
+//    /**
+//     * IDs of all special ability cards
+//     *  if the cards are in a player's hand, then it is taken out of the deck
+//     * FOR PREMIUM GAME ONLY
+//     * FOR BASIC: is a non-initialized array errors thrown when trying to access it
+//     */
+//    private ArrayList<Integer> abilityDeck;
 
-    private int turnNum = 0;
+    @Column(name = "weapons")
+    @NotFound(action = NotFoundAction.IGNORE)
+    /**
+     * Card IDs for the weapons
+     */
+    private ArrayList<Integer> weapons;
+
+    @Column(name = "suspects")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private ArrayList<Integer> suspects;
+
+    @Column(name = "rooms")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private ArrayList<Integer> rooms;
+
+//    @Column(name = "turnOrder")
+//    @NotFound(action = NotFoundAction.IGNORE)
+//    private ArrayList<Integer> turnOrder;
+//    private ArrayList<Integer> deck;
+    private Random rand;
+    private Integer turnNum;
 
     public GameState() {
         finalCardIDs = new Integer[3];
-        turnOrder = new ArrayList<>();
-//        playerHand = new ArrayList<>();
+//        turnOrder = new ArrayList<>();
+        rand = new Random();
+        weapons = new ArrayList<>();
+        rooms = new ArrayList<>();
+        suspects = new ArrayList<>();
+        turnNum = 0;
     }
 
     public GameState(Integer versionID) {
         this.versionID = versionID;
     }
 
-    public Integer getNextPlayer() {
-        Integer tmp = turnOrder.get(turnNum);
-        if(turnNum == turnOrder.size()-1) {turnNum = 0;}
-        else {turnNum += 1;}
-        return tmp;
-    }
-//    public ArrayList<Integer> getPlayerHand(int playerID) {
-//        for(int i = 0; i < turnOrder.size(); i++) {
-//            if (turnOrder.get(i) == playerID) {
-//                return playerHand.get(i);
-//            }
-//        }
-//        return null;
+//    public Integer getNextPlayer() {
+//        Integer tmp = turnOrder.get(turnNum);
+//        if(turnNum == turnOrder.size()-1) {turnNum = 0;}
+//        else {turnNum += 1;}
+//        return tmp;
 //    }
-    public Boolean checkFinalGuess(int[] guess) {
+
+    public Boolean checkFinalGuess(Integer[] guess) {
         for (int i = 0; i < finalCardIDs.length; i++) {
             if(guess[i] != finalCardIDs[i]) {return false;}
         }
         return true;
     }
+
+    public void setFinalCardIDs() {
+        int weapon = rand.nextInt(weapons.size());
+        int suspect = rand.nextInt(suspects.size());
+        int room = rand.nextInt(rooms.size());
+
+        finalCardIDs[0] = weapons.get(weapon);
+        finalCardIDs[1] = suspects.get(suspect);
+        finalCardIDs[2] = rooms.get(room);
+
+        weapons.remove(weapon);
+        suspects.remove(suspect);
+        rooms.remove(room);
+
+    }
     public Integer[] getFinalCardIDs() {return finalCardIDs;}
     public Integer getVersionID() {return versionID;}
     public Integer getID() {return ID;}
     public Integer getLobbyID() {return lobbyID;}
+    public ArrayList<Integer> getWeapons() {return weapons;}
+    public ArrayList<Integer> getSuspects() {return suspects;}
+    public ArrayList<Integer> getRooms() {return rooms;}
 }
--- Backend/clue_backend/src/main/java/tz_7/GamePlay/GameStateDatabase/GameStateController.java
+++ Backend/clue_backend/src/main/java/tz_7/GamePlay/GameStateDatabase/GameStateController.java
@@ -3,7 +3,12 @@ package tz_7.GamePlay.GameStateDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tz_7.GamePlay.GameLobbyDatabase.GameLobby;
+
+import java.util.List;
 
 @RestController
 public class GameStateController {
@@ -11,8 +16,36 @@ public class GameStateController {
     GameStateRepository stateRepository;
     private final Logger logger = LoggerFactory.getLogger(GameStateRepository.class);
 
+    /**
+     * Post mapping that creates a new Game State
+     * @param state
+     *  The json params for a game state
+     * @return
+     *  GameState Entity
+     */
     @PostMapping(value = "game/new", consumes = "application/json")
-    public GameState newGameState(@RequestBody GameState state) {
+    public GameState newState(@RequestBody GameState state) {
+        state.setFinalCardIDs();
         return stateRepository.save(state);
     }
+
+    @GetMapping(value = "game")
+    public ResponseEntity<List<GameState>> getAllLobbies() {
+        return new ResponseEntity<List<GameState>>(stateRepository.findAll(), HttpStatus.OK);
+    }
+
+    /**
+     * Get mapping to verify if the user's final guess is correct
+     * @param guess
+     *  Array of card ids
+     * @return
+     *  true - if all the ids match
+     *  false - if the ids don't match
+     */
+    @GetMapping(value = "game/checkGuess", consumes = "application/json")
+    public Boolean checkGuess (@RequestBody Integer[] guess, @RequestBody Integer gameID) {
+        GameState state = stateRepository.getById(gameID);
+        return state.checkFinalGuess(guess);
+    }
+
 }
