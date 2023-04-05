9f01db75f8af02b2ba6bf84c9b1b88b60a68fd68
Piper Ideker <plideker@iastate.edu>
2023-04-04 19:20:28 -0500

    added board

--- Clue_Frontend/app/src/main/java/com/example/clue_frontend/Tile.java
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/Tile.java
@@ -6,6 +6,17 @@ public class Tile {
     private Bitmap bm;
     private int x ,y, width, height;
 
+    public Bitmap getBm() {
+        return bm;
+    }
+
+    public int getTileX() {
+        return x;
+    }
+
+    public int getTileY() {
+        return y;
+    }
     public Tile(Bitmap bm, int x, int y, int width, int height){
         this.bm = bm;
         this.x = x;
@@ -14,44 +25,6 @@ public class Tile {
         this.height = height;
     }
 
-    public Bitmap getBm() {
-        return bm;
-    }
 
-    public void setBm(Bitmap bm) {
-        this.bm = bm;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
 }
 
