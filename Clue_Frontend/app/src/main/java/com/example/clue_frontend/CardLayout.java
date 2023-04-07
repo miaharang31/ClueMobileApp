6514f9cf860fd0dbbebf3822c2cffdcb46d74b82
gbrickey <gbrickey@iastate.edu>
2023-04-07 14:29:20 -0500

    creating card layout for users

---
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/CardLayout.java
@@ -0,0 +1,7 @@
+package com.example.clue_frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CardLayout extends AppCompatActivity {
+
+}
--- Clue_Frontend/app/src/main/java/com/example/clue_frontend/Home.java
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/Home.java
@@ -41,33 +41,33 @@ public class Home extends AppCompatActivity {
         host = (Button)findViewById(R.id.host);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.profile){
-                    Toast.makeText(Home.this, "Profile Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                if(item.getItemId() == R.id.settings){
-                    Toast.makeText(Home.this, "Settings Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                if(item.getItemId() == R.id.howToPlay){
-                    Toast.makeText(Home.this, "How To Play Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                if(item.getItemId() == R.id.logout){
-                    Toast.makeText(Home.this, "Logout Selected", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                return false;
-            }
-        });
+//        navigationView = findViewById(R.id.nav_view);
+//        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+//        drawerLayout.addDrawerListener(drawerToggle);
+//        drawerToggle.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                if(item.getItemId() == R.id.profile){
+//                    Toast.makeText(Home.this, "Profile Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//                if(item.getItemId() == R.id.settings){
+//                    Toast.makeText(Home.this, "Settings Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//                if(item.getItemId() == R.id.howToPlay){
+//                    Toast.makeText(Home.this, "How To Play Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//                if(item.getItemId() == R.id.logout){
+//                    Toast.makeText(Home.this, "Logout Selected", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
 
         join.setOnClickListener(new View.OnClickListener(){
             @Override
