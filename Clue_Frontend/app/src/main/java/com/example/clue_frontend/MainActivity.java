beb50240cfd90e4507a68a1419c0817d2b9a5f9b
gbrickey <gbrickey@iastate.edu>
2023-04-19 12:33:54 -0500

    adding card fix and changing other things (put the activity main intent back to home for error)

--- Clue_Frontend/app/src/main/java/com/example/clue_frontend/MainActivity.java
+++ Clue_Frontend/app/src/main/java/com/example/clue_frontend/MainActivity.java
@@ -30,6 +30,7 @@ import com.example.clue_frontend.GamePlay.CharacterSelection;
 import com.example.clue_frontend.GamePlay.Constraints;
 import com.example.clue_frontend.GamePlay.Game;
 import com.example.clue_frontend.GamePlay.GameView;
+import com.example.clue_frontend.Lobbies.HostLobby;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -78,8 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     username.setError(null);
                     checkUsername = true;
-
-            }
+                }
                 if(usernameData.isEmpty()){
                     username.setError("Username cannot be empty");
                     checkUsername = false;
@@ -87,23 +87,19 @@ public class MainActivity extends AppCompatActivity {
                     username.setError(null);
                     checkUsername = true;
 
-            if(passwordData.isEmpty()){
-                password.setError("Password cannot be empty");
-                checkPassword = false;
-            }else{
-                password.setError(null);
-                checkPassword = true;
-            }
-
+                    if(passwordData.isEmpty()){
+                        password.setError("Password cannot be empty");
+                        checkPassword = false;
+                    }else{
+                        password.setError(null);
+                        checkPassword = true;
+                    }
 
                     // if all textboxes are correct, all data will be added to the SecondActivity (main page for either regular/premium users) and will start
-                    JsonObjectRequest jreq = null;
-                    if (checkUsername == true && checkPassword == true) {
-
+                    if (checkUsername == true && checkPassword == true){
                         String url = "http://coms-309-038.class.las.iastate.edu:8080/login";
                         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                         JSONObject json = null;
-
                         try {
                             json = new JSONObject();
                             json.put("username", usernameData);
@@ -111,33 +107,75 @@ public class MainActivity extends AppCompatActivity {
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        jreq = new JsonObjectRequest(Request.Method.POST, url, json,
-                                new Response.Listener<JSONObject>() {
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        MyApplication app = (MyApplication) getApplication();
-                                        try {
-                                            app.setUserid((Integer) response.get("id"));
-                                        } catch (JSONException e) {
-                                            throw new RuntimeException(e);
-                                        }
-                                        startActivity(intent);
-                                    }
-                                },
-                                new Response.ErrorListener() {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
-                                        Toast.makeText(MainActivity.this, "Error: " + error, Toast.LENGTH_SHORT).show();
-                                        Log.d("ResponseError", error.toString());
-                                    }
-                                });
 
-                        queue.add(jreq);
+                        if (passwordData.isEmpty()) {
+                            password.setError("Password cannot be empty");
+                            checkPassword = false;
+                        } else {
+                            password.setError(null);
+                            checkPassword = true;
+                        }
+
+                        // if all textboxes are correct, all data will be added to the SecondActivity (main page for either regular/premium users) and will start
+                        if (checkUsername == true && checkPassword == true) {
+                            try {
+                                json = new JSONObject();
+                                json.put("username", usernameData);
+                                json.put("password", passwordData);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            if (passwordData.isEmpty()) {
+                                password.setError("Password cannot be empty");
+                                checkPassword = false;
+                            } else {
+                                password.setError(null);
+                                checkPassword = true;
+                            }
+
+                            // if all textboxes are correct, all data will be added to the SecondActivity (main page for either regular/premium users) and will start
+                            JsonObjectRequest jreq = null;
+                            if (checkUsername == true && checkPassword == true) {
+                                try {
+                                    json = new JSONObject();
+                                    json.put("username", usernameData);
+                                    json.put("password", passwordData);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                jreq = new JsonObjectRequest(Request.Method.POST, url, json,
+                                        new Response.Listener<JSONObject>() {
+                                            @Override
+                                            public void onResponse(JSONObject response) {
+                                                MyApplication app = (MyApplication) getApplication();
+                                                try {
+                                                    app.setUserid((Integer) response.get("id"));
+                                                } catch (JSONException e) {
+                                                    throw new RuntimeException(e);
+                                                }
+                                                startActivity(intent);
+                                            }
+                                        },
+                                        new Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                Toast.makeText(MainActivity.this, "Error: " + error, Toast.LENGTH_SHORT).show();
+                                                Log.d("ResponseError", error.toString());
+                                            }
+                                        });
+
+                                queue.add(jreq);
+                            }
+
+
+                            queue.add(jreq);
+                        }
+
                     }
-
-                }
-            });
-
+                };
+            }
+        });
 
         //when the new player link is clicked, go to th SignUpActivity
         newPlayerLink.setOnClickListener(new View.OnClickListener() {
@@ -152,9 +190,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-                }
-            }
-
-
-
-
+    }
+}
