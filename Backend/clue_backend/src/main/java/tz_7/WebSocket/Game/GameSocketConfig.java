ac9a558dfe1d57f1444e2b1e035bd7337f885a95
Mia Harang <miharang@iastate.edu>
2023-04-30 00:28:11 -0500

    edit game

---
+++ Backend/clue_backend/src/main/java/tz_7/WebSocket/Game/GameSocketConfig.java
@@ -0,0 +1,13 @@
+package tz_7.WebSocket.Game;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+public class GameSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
