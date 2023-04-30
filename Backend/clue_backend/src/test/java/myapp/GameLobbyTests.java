31b143033a2efa35ee81b6c4c891f5bf01e28f79
Mia Harang <miharang@iastate.edu>
2023-04-30 01:45:36 -0500

    start testing

---
+++ Backend/clue_backend/src/test/java/myapp/GameLobbyTests.java
@@ -0,0 +1,61 @@
+package myapp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+/**
+ * Author: Mia Harang
+ *  System test to test the game lobby
+ */
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class GameLobbyTests {
+    @LocalServerPort
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+    }
+
+//    TODO: Make more tests
+    @Test
+    public void reverseTest() {
+        Response response = RestAssured.given().
+                header("Content-Type", "text/plain").
+                header("charset","utf-8").
+                body("hello").
+                when().
+                post("/reverse");
+
+
+        // Check status code
+        int statusCode = response.getStatusCode();
+        assertEquals(200, statusCode);
+
+        // Check response body for correct response
+        String returnString = response.getBody().asString();
+        try {
+            JSONArray returnArr = new JSONArray(returnString);
+            JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
+            assertEquals("olleh", returnObj.get("data"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
