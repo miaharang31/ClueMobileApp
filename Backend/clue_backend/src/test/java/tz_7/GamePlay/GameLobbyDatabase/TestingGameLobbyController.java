10ebc2e9340590143289fca6f81773e87fd16b09
Mia Harang <miharang@iastate.edu>
2023-04-30 19:57:41 -0500

    testing

--- Backend/clue_backend/src/test/java/tz_7/GamePlay/GameLobbyDatabase/TestingGameLobbyController.java
+++ Backend/clue_backend/src/test/java/tz_7/GamePlay/GameLobbyDatabase/TestingGameLobbyController.java
@@ -6,8 +6,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
@@ -15,10 +21,11 @@ import io.restassured.response.Response;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import tz_7.PlayerDatabase.Player;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 public class TestingGameLobbyController {
 
@@ -32,7 +39,7 @@ public class TestingGameLobbyController {
 	}
 
 	@Test
-	public void runTest() {
+	public void a_runTest() {
 		// Send request and receive response
 		Response response = RestAssured.given().
 				header("Content-Type", "text/plain").
@@ -46,29 +53,128 @@ public class TestingGameLobbyController {
 	}
 
 	@Test
-	public void capitalizeTest() {
-		// Send request and receive response
-//		Response response = RestAssured.given().
-//				header("Content-Type", "text/plain").
-//				header("charset","utf-8").
-//				body("hello").
-//				when().
-//				post("/capitalize");
+	public void b_createLobbyTest() {
+		Player player = new Player("test", "test", "test@test.test", "test", "test", "b");
+		GameLobby lobby = new GameLobby(2, "test", player, false);
+		Response response = RestAssured.given().
+				header("Content-Type", "application/json").
+				header("charset","utf-8").
+				body(lobby).
+				when().
+				post("/lobby/new/{hostid}", 1);
 
+		int statusCode = response.getStatusCode();
+		assertEquals(200, statusCode);
 
-		// Check status code
-//		int statusCode = response.getStatusCode();
-//		assertEquals(200, statusCode);
-//
-//		// Check response body for correct response
-//		String returnString = response.getBody().asString();
-//		try {
-//			JSONArray returnArr = new JSONArray(returnString);
-//			JSONObject returnObj = returnArr.getJSONObject(returnArr.length()-1);
-//			assertEquals("HELLO", returnObj.get("data"));
-//		} catch (JSONException e) {
-//			e.printStackTrace();
-//		}
+		String returnLobby = response.getBody().asString();
+		try {
+			JSONObject object = new JSONObject(returnLobby);
+			JSONArray arr = (JSONArray) object.get("players");
+			assertEquals(0, arr.length());
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void d_addPlayersTest() {
+		Response response = RestAssured.given().
+				header("charset","utf-8").
+				when().
+				put("lobby/join/{playerID}/code/{gamecode}", 5, "test");
+
+		int statusCode = response.getStatusCode();
+		assertEquals(200, statusCode);
+
+		String returnLobby = response.getBody().asString();
+		try {
+			JSONObject object = new JSONObject(returnLobby);
+			JSONArray arr = (JSONArray) object.get("players");
+			assertEquals(1, arr.length());
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+@Test
+	public void e_addPlayerMaxTest() {
+		Response response = RestAssured.given().
+				header("charset","utf-8").
+				when().
+				put("lobby/join/{playerID}/code/{gamecode}", 4, "test");
+
+		int statusCode = response.getStatusCode();
+		assertEquals(200, statusCode);
+
+		String returnLobby = response.getBody().asString();
+		try {
+			JSONObject object = new JSONObject(returnLobby);
+			JSONArray arr = (JSONArray) object.get("players");
+			assertEquals(1, arr.length());
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void e_getHostTest() {
+		int id;
+		Response response = RestAssured.given().
+				header("charset","utf-8").
+				when().
+				get("/lobby/find/host/{id}", 1);
+
+		String s = response.getBody().asString();
+		try {
+			JSONObject obj = new JSONObject(s);
+			id = (int) obj.get("id");
+			response = RestAssured.given().
+					header("charset", "utf-8").
+					when().
+					get("lobby/host/{gameLobbyID}", id);
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+
+		s = response.getBody().asString();
+		try {
+			JSONObject object = new JSONObject(s);
+			int playerid = (int) object.get("id");
+			assertEquals(1, playerid);
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void z_deleteLobbyTest() {
+		int id;
+		Response response = RestAssured.given().
+				header("charset","utf-8").
+				when().
+				get("/lobby/find/host/{id}", 1);
+
+		String s = response.getBody().asString();
+		try {
+			JSONObject obj = new JSONObject(s);
+			id = (int) obj.get("id");
+			response = RestAssured.given().
+					header("charset","utf-8").
+					when().
+					delete("/lobby/delete/{id}", id);
+
+			int statusCode = response.getStatusCode();
+			assertEquals(200, statusCode);
+
+			response = RestAssured.given().
+					header("charset","utf-8").
+					when().
+					delete("/lobby/{id}", id);
+			statusCode = response.getStatusCode();
+			assertEquals(405, statusCode);
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+
 
 	}
 }
